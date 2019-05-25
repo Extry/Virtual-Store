def7cf0bcdd8f07e7e276a3f5db3f5cebb4c3cdb
Extry <45901764+Extry@users.noreply.github.com>
2019-05-25 18:47:37 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php 
+ 
+define('default_lang', 'pt-br');
+define('BASE_URL', 'http://localhost/virtual/');
+define('db', ['dbname'=> 'virtual', 'host'=> 'localhost', 'dbuser'=> 'root', 'dbpass'=> '']);
