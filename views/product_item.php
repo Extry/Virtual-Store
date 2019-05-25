0269ac5088ebf2f80a4f631845db047d9c612302
Extry <45901764+Extry@users.noreply.github.com>
2019-05-25 18:54:10 -0300

    Create product_item.php

---
+++ views/product_item.php
@@ -0,0 +1,7 @@
+<div class="product_item">
+	<div class="product_image">
+		<img src="<?php echo BASE_URL; ?>media/products/1.jpg" width="100%">
+	</div>
+	<div class="product_name">		Monitor cuzin	</div>
+	<div class="product_brand">	Sonic	</div>
+</div>
