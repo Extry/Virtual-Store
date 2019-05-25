4dc5f8f52582afb2afbd52b5c104b48d23eac72d
Extry <45901764+Extry@users.noreply.github.com>
2019-05-25 18:54:02 -0300

    Create 404.php

---
+++ views/404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Página não encontrada</title>
+	<link rel="stylesheet" href="<?php echo BASE_URL;?>">
+</head>
+<body>
+	Página não encontrada mother fuck
+</body>
+</html>
