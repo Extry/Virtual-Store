6d6dd1650724710eb3855701ff9263c310349931
Extry <45901764+Extry@users.noreply.github.com>
2019-05-25 18:54:06 -0300

    Create home.php

---
+++ views/home.php
@@ -0,0 +1,11 @@
+<div class="row">
+	<div class="col-sm-4">
+		<?php $this->loadView('product_item', []); ?>
+	</div>
+	<div class="col-sm-4">
+		
+	</div>
+	<div class="col-sm-4">
+		
+	</div>
+</div>
