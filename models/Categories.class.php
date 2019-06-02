5015ffd348b967d71ed1bac83c6e73e55e2f9f09
Extry <45901764+Extry@users.noreply.github.com>
2019-06-01 23:41:22 -0300

    Create Categories.class.php

---
+++ models/Categories.class.php
@@ -0,0 +1,48 @@
+<?php 
+
+class Categories extends Model
+{
+
+	public function getList()
+	{
+		$array = [];
+		$sql = $this->pdo->query('SELECT * FROM categories ORDER BY sub DESC');
+		if($sql->rowCount() > 0){
+			foreach ($sql->fetchAll() as $item) {
+				$item['subs'] = [];
+				$array[$item['id']] = $item;
+			}
+			while ($this->stillNeed($array)) {
+				$this->organizeCategory($array);
+			}
+
+		}
+ 
+		return $array;  	
+	}
+
+	private function organizeCategory(&$array)
+	{
+		foreach ($array as $id => $item) {
+			if(isset($array[$item['sub']])){
+				$array[$item['sub']]['subs'][$item['id']] = $item;
+				unset($array[$id]);
+				break;
+			}
+		}
+
+	} 
+
+	private function stillNeed($array)
+	{
+		foreach($array as $item){
+			if(!empty($item['sub'])){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+}
+
