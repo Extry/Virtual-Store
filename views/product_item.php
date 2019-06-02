3fab2db0fc7bc1fec6aa9713cdba894099e19f1f
Extry <45901764+Extry@users.noreply.github.com>
2019-06-01 23:43:29 -0300

    Update product_item.php

--- views/product_item.php
+++ views/product_item.php
@@ -1,7 +1,30 @@
 <div class="product_item">
-	<div class="product_image">
-		<img src="<?php echo BASE_URL; ?>media/products/1.jpg" width="100%">
+	<a href="<?php echo BASE_URL; ?>">
+		<div class="product_tags">
+			<?php if ($sale == 1):?>
+				<div class="product_tag  product_tag_red"><?php echo $this->lang->get('SALE'); ?></div>
+			<?php endif; ?>
+			<?php if ($bestseller == 1): ?>
+				<div class="product_tag product_tag_green"><?php echo $this->lang->get('BESTSELLER'); ?></div>
+			<?php endif; ?>
+			<?php if ($new_product == 1): ?>
+				<div class="product_tag product_tag_blue"><?php echo $this->lang->get('NEW'); ?></div>
+			<?php endif; ?>
+
+		</div>
+		<div class="product_image">
+			<img src="<?php echo BASE_URL; ?>media/products/<?php echo $images[0]['url']; ?>" width="100%">
+		</div>
+		<div class="product_name"><?php echo $name;?> </div>
+		<div class="product_brand"><?php echo $brand_name; ?> </div>
+		<div class="product_price_from">
+			<?php 
+			if($price_from != 0){
+				echo 'R$ '.number_format($price_from, 2, ',', '.');
+			}?>
+
+			</div>
+			<div class="product_price">R$ <?php echo number_format($price, 2, ',', '.'); ?></div>
+			<div style="clear:both"></div>
+		</a>
 	</div>
-	<div class="product_name">		Monitor cuzin	</div>
-	<div class="product_brand">	Sonic	</div>
-</div>
