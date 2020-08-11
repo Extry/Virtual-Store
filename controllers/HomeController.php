2fec941e12ab80829c358de24a018c26f820f21e
Extry <45901764+Extry@users.noreply.github.com>
2020-08-11 13:42:03 -0300

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -5,12 +5,23 @@ class HomeController extends Controller
 
 	public function index()
 	{
+		$store  = new Store();
 		$products = new Products();
 		$categories = new Categories();
+		$f = new Filters();
+
+		$data = $store->getTemplateData();
+		
+
+		$filters = [];
+
+		if(!empty($_GET['filter']) && is_array($_GET['filter'])){
+			$filters = $_GET['filter'];
+
+		}
 		$currentPage = 1;
 		$offset = 0;
-		$limit = 5;
-
+		$limit = 3;
 		if(!empty($_GET['p'])){
 			$currentPage = $_GET['p'];
 		}
@@ -18,13 +29,20 @@ class HomeController extends Controller
 			header("Location: ".BASE_URL."?p=1");
 		}
 		$offset = ($currentPage * $limit) - $limit;
-		$date['list'] = $products->getList($offset, $limit);
-		$date['totalitens'] = $products->getTotal();
-		$date['number'] = ceil($date['totalitens'] / $limit);
-		$date['currentPage'] = $currentPage;
-		$date['categories'] = $categories->getList();
+		$data['list'] = $products->getList($offset, $limit,$filters);
+		$data['totalitens'] = $products->getTotal($filters);
+		$data['number'] = ceil($data['totalitens'] / $limit);
+		$data['currentPage'] = $currentPage;
+		$data['filters'] = $f->getFilters($filters);
+		$data['filters_selected'] = $filters;
+		$data['searchTerm'] = '';
+		$data['category'] = '';
+		$data['sidebar'] = true;
+		
 
-		$this->loadTemplate('home', $date);
+		$this->loadTemplate('home', $data);
 	}
-	 
+	
 }
+
+
