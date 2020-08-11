dc36f3f9d4537c8651a980a10c68e88e0819ebf8
Extry <45901764+Extry@users.noreply.github.com>
2020-08-11 13:50:48 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,3 +3,4 @@
 define('default_lang', 'pt-br');
 define('BASE_URL', 'http://localhost/virtual/');
 define('db', ['dbname'=> 'virtual', 'host'=> 'localhost', 'dbuser'=> 'root', 'dbpass'=> '']);
+define('config', ['cep_origin' => '...']);
