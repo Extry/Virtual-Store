c54918c89ec6ef2ed3b3ba0a2a882198f2bd7c98
Extry <45901764+Extry@users.noreply.github.com>
2020-08-11 13:52:38 -0300

    Update Brands.class.php

--- models/Brands.class.php
+++ models/Brands.class.php
@@ -3,18 +3,27 @@
 class Brands extends Model
 {
 	
+	public function getList()
+	{
+		$array = [];
+		$sql = $this->pdo->query("SELECT * FROM brands");
+		if($sql->rowCount() >0){
+			$array = $sql->fetchAll();
+		}
+		return $array;
+	}
+
 	public function getNameById($id)
 	{
 		$sql = $this->pdo->prepare('SELECT name FROM WHERE id = ?');
 		$sql->execute([$id]);
 		if($sql->rowCount() > 0){
 			$data = $sql->fetch();
-
 			return $data['name'];
 		}else{
 			return '';
 		}
 	}
 
-
 }
+
